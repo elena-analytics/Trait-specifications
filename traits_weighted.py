2082791a8df145a788dca9019022a90c5958c663
Elena <elena.michelli@gmail.com>
2026-03-15 23:32:13 -0400

    Add metadata comments to traits_weighted.py

---
+++ traits_weighted.py
@@ -0,0 +1,11 @@
+# Trait weighting configuration
+# Used by the hot-nerd-detector model
+# Elena Analytics Research Lab
+
+traits = {
+    "nerd_vibe": 3,
+    "messy_blonde_hair": 3,
+    "colorful_eyes": 2,
+    "glasses": 2,
+    "lanky": 2,
+}
